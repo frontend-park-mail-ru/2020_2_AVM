8ccc93de20cc0b7b7bd5d7c8c1055a561d708dba
Rufina <32850270+rufa-a@users.noreply.github.com>
2020-10-06 21:42:47 +0300

    Validation (#6)
    * first simple validation for login
    
    * some fixes
    
    * added some validation
    
    * validation for login and authorization

--- static/js/components/LoginPage/LoginPage.js
+++ static/js/components/LoginPage/LoginPage.js
@@ -2,6 +2,8 @@ import Methods from '../../api/methods.js';
 
 import {profilePage} from '../../views/ProfilePage/profilePage.js';
 
+import Validation from '../../utils/validation.js';
+
 export class LoginPage {
     #parent
     #data
@@ -23,18 +25,47 @@ export class LoginPage {
         this.#parent.innerHTML = window.fest['js/components/LoginPage/LoginPage.tmpl'](this.#data);
     }
 
+    addErrorMsg(divMsgError) {
+        const msgLabel = document.createElement("Label");
+        msgLabel.innerHTML = 'Длина логина должна быть не менее 8 символов';
+        msgLabel.style.color = 'red';
+        divMsgError.appendChild(msgLabel);
+    }
+
+    checkLogin(divMsgError, loginInput, passwordInput) {
+        const login = loginInput.value.trim();
+        const password = passwordInput.value.trim();
+
+        const checkLoginLength = Validation.validateLoginLength(login);
+            if (!checkLoginLength) {
+                this.addErrorMsg(divMsgError);
+                return void 0;
+            }
+
+        return {login, password};
+    }
+
     submitForm() {
         const form = document.querySelector('form#form-login');
         const loginInput = form.querySelector('input[type="email"]');
         const passwordInput = form.querySelector('input[type="password"]');
 
+        const divMsgError = document.createElement('div');
+        form.parentNode.insertBefore(divMsgError, form);
+
         form.addEventListener('submit', (evt) => {
             evt.preventDefault();
 
-            const login = loginInput.value.trim();
-            const password = passwordInput.value.trim();
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
 
-            Methods.makeLogin({login, password})
+            const authData = this.checkLogin(divMsgError, loginInput, passwordInput);
+            if (authData !== undefined) {
+                return;
+            }            
+
+            Methods.makeLogin(authData)
                 .then(({statusCode, responseObject}) => {
                     if (statusCode === 200) {
                         profilePage(this.#parent);
@@ -50,6 +81,24 @@ export class LoginPage {
                     this.#data.login = true;
                     this.render();
                 });
+        })
+
+        loginInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkLogin(divMsgError, loginInput, passwordInput);
+        })
+
+        passwordInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkLogin(divMsgError, loginInput, passwordInput);
         })
     }
 
--- static/js/components/SignUpPage/SignUpPage.js
+++ static/js/components/SignUpPage/SignUpPage.js
@@ -1,6 +1,8 @@
 import Methods from "../../api/methods.js";
 import {profilePage} from "../../views/ProfilePage/profilePage.js";
 
+import Validation from '../../utils/validation.js';
+
 export class SignUpPage {
     #parent
     #data
@@ -22,6 +24,92 @@ export class SignUpPage {
         this.#parent.innerHTML = window.fest['js/components/SignUpPage/SignUpPage.tmpl'](this.#data);
     }
 
+    addErrorMsg(divMsgError, inputError) {
+        const msgLabel = document.createElement("Label");
+        if (inputError == 'Login'){
+            msgLabel.innerHTML = 'Длина логина должна быть не менее 8 символов, логин не может содержать специальные символы';
+            msgLabel.style.color = 'red';            
+        }
+        if (inputError == 'Email'){
+            msgLabel.innerHTML = 'Неверно введена почта';
+            msgLabel.style.color = 'red';            
+        }
+        if (inputError == 'empty or invalid'){
+            msgLabel.innerHTML = 'Пароль не может быть пустым';
+            msgLabel.style.color = 'red';            
+        }
+        if (inputError == 'short'){
+            msgLabel.innerHTML = 'Длина пароля должна быть не менее 8 символов';
+            msgLabel.style.color = 'red';            
+        }
+        if (inputError == 'no nums'){
+            msgLabel.innerHTML = 'В пароле должны содержаться десятичные цифры';
+            msgLabel.style.color = 'red';            
+        }
+        if (inputError == 'few nums'){
+            msgLabel.innerHTML = 'В пароле должно содержаться более двух десятичных цифр';
+            msgLabel.style.color = 'red';            
+        }
+        if (inputError == 'no latin'){
+            msgLabel.innerHTML = 'В пароле должны содержатся латинские буквы';
+            msgLabel.style.color = 'red';            
+        }
+        if (inputError == 'nums and latin'){
+            msgLabel.innerHTML = 'В пароле должны содержатся латинские буквы и десятичные цифры';
+            msgLabel.style.color = 'red';            
+        }
+        if (inputError == 'PassRep'){
+            msgLabel.innerHTML = 'Пароли не совпадают';
+            msgLabel.style.color = 'red';            
+        }
+        divMsgError.appendChild(msgLabel);
+        
+    }
+
+    checkSignup(divMsgError, loginInput, emailInput, avatarInput, quoteInput, quoteAuthorInput, aboutInput, passwordInput, passwordRepeatInput) {
+        const login = loginInput.value.trim();
+        const email = emailInput.value.trim();
+        const avatar = avatarInput.value.trim();
+        const quote = quoteInput.value.trim();
+        const quoteAuthor = quoteAuthorInput.value.trim();
+        const about = aboutInput.value.trim();
+        const password = passwordInput.value.trim();
+        const passwordRepeat = passwordRepeatInput.value.trim();
+
+        const checkLogin = Validation.validateLogin(login);
+        var notValidated = false;
+        if (!checkLogin) {
+            this.addErrorMsg(divMsgError, 'Login');
+            notValidated = true;
+        }
+
+        const checkEmail = Validation.validateEmail(email);
+        if (!checkEmail) {
+            this.addErrorMsg(divMsgError, 'Email');
+            notValidated = true;
+        }
+
+        const checkPass = Validation.validatePassword(password);
+        if (typeof checkPass !== 'undefined' && checkPass.length > 0) {
+            checkPass.forEach(errPass => {
+                this.addErrorMsg(divMsgError, errPass);
+            });
+            notValidated = true;
+        }
+
+        const checkPassRepeat = Validation.validatePasswordRepeat(password, passwordRepeat);
+        if (!checkPassRepeat) {
+            this.addErrorMsg(divMsgError, 'PassRep');
+            notValidated = true;
+        }
+
+        if (notValidated) {
+            return void 0;
+        }
+
+        return {login, email, avatar, quote, quoteAuthor, about, password, passwordRepeat};
+    }
+
     submitForm() {
         const form = document.querySelector('form#form-sing-up');
         const loginInput = form.querySelector('input#inputLogin');
@@ -33,28 +121,21 @@ export class SignUpPage {
         const passwordInput = form.querySelector('input#password');
         const passwordRepeatInput = form.querySelector('input#passwordRepeat');
 
+        const divMsgError = document.createElement('div');
+        form.parentNode.insertBefore(divMsgError, form);
+
         form.addEventListener('submit', (evt) => {
             evt.preventDefault();
 
-            const login = loginInput.value.trim();
-            const email = emailInput.value.trim();
-            const avatar = avatarInput.value.trim();
-            const quote = quoteInput.value.trim();
-            const quoteAuthor = quoteAuthorInput.value.trim();
-            const about = aboutInput.value.trim();
-            const password = passwordInput.value.trim();
-            const passwordRepeat = passwordRepeatInput.value.trim();
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            const regData = this.checkSignup(divMsgError, loginInput, emailInput, avatarInput, quoteInput, quoteAuthorInput, aboutInput, passwordInput, passwordRepeatInput);
+            if (!regData) {
+                return;
+            }
 
-            Methods.makeSignUp({
-                login,
-                email,
-                avatar,
-                quote,
-                quoteAuthor,
-                about,
-                password,
-                passwordRepeat,
-            })
+            Methods.makeSignUp(regData)
                 .then(({statusCode, responseObject}) => {
                     if (statusCode === 200) {
                         profilePage(this.#parent);
@@ -70,6 +151,93 @@ export class SignUpPage {
                     this.#data.sign = true;
                     this.render();
                 });
+        })
+
+        const formInputs = form.children;
+
+        // formInputs.forEach(input => {
+        //     input.addEventListener('blur', (evt) => {
+        //         evt.preventDefault();
+    
+        //         console.log("BLUR");
+    
+        //         while (divMsgError.firstChild) {
+        //             divMsgError.removeChild(divMsgError.lastChild);
+        //         }
+        //         this.checkSignup(divMsgError, loginInput, emailInput, avatarInput, quoteInput, quoteAuthorInput, aboutInput, passwordInput, passwordRepeatInput, form);
+        //     })
+        // })
+
+        loginInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkSignup(divMsgError, loginInput, emailInput, avatarInput, quoteInput, quoteAuthorInput, aboutInput, passwordInput, passwordRepeatInput, form);
+        })
+
+        emailInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkSignup(divMsgError, loginInput, emailInput, avatarInput, quoteInput, quoteAuthorInput, aboutInput, passwordInput, passwordRepeatInput, form);
+        })
+
+        avatarInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkSignup(divMsgError, loginInput, emailInput, avatarInput, quoteInput, quoteAuthorInput, aboutInput, passwordInput, passwordRepeatInput, form);
+        })
+
+        quoteInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkSignup(divMsgError, loginInput, emailInput, avatarInput, quoteInput, quoteAuthorInput, aboutInput, passwordInput, passwordRepeatInput, form);
+        })
+
+        quoteAuthorInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkSignup(divMsgError, loginInput, emailInput, avatarInput, quoteInput, quoteAuthorInput, aboutInput, passwordInput, passwordRepeatInput, form);
+        })
+
+        aboutInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkSignup(divMsgError, loginInput, emailInput, avatarInput, quoteInput, quoteAuthorInput, aboutInput, passwordInput, passwordRepeatInput, form);
+        })
+
+        passwordInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkSignup(divMsgError, loginInput, emailInput, avatarInput, quoteInput, quoteAuthorInput, aboutInput, passwordInput, passwordRepeatInput, form);
+        })
+
+        passwordRepeatInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkSignup(divMsgError, loginInput, emailInput, avatarInput, quoteInput, quoteAuthorInput, aboutInput, passwordInput, passwordRepeatInput, form);
         })
     }
 
