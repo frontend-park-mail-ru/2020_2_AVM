61cf1b82ce342fbc86532fe9849a47b2b51cee8b
rufa-a <roofinda@gmail.com>
2020-10-07 11:34:13 +0300

    created login and signup controllers

---
+++ static/js/controllers/login-controller.js
@@ -0,0 +1,113 @@
+import UserModel from '../../js/models/user-model.js'
+
+import {profilePage} from '../views/ProfilePage/profilePage.js';
+
+import Controller from '../api/controller.js'
+
+import Validation from '../utils/validation.js'
+
+export default class LoginController extends Controller {
+    #parent
+    #data
+
+    constructor(parent) {
+        super();
+
+        this.#parent = parent;
+        this.#data = {};
+    }
+
+    get data() {
+        return this.#data;
+    }
+
+    set data(data) {
+        this.#data = data;
+    }
+
+    // TODO: вынести в LoginView
+    render() {
+        this.#parent.innerHTML = window.fest['js/components/LoginPage/LoginPage.tmpl'](this.#data);
+    }
+
+    addErrorMsg(divMsgError) {
+        const msgLabel = document.createElement("Label");
+        msgLabel.innerHTML = 'Длина логина должна быть не менее 8 символов';
+        msgLabel.style.color = 'red';
+        divMsgError.appendChild(msgLabel);
+    }
+
+    checkLogin(divMsgError, loginInput, passwordInput) {
+        const login = loginInput.value.trim();
+        const password = passwordInput.value.trim();
+
+        const checkLoginLength = Validation.validateLoginLength(login);
+            if (!checkLoginLength) {
+                this.addErrorMsg(divMsgError);
+                return void 0;
+            }
+
+        return {login, password};
+    }
+
+    action() {
+        this.render();
+
+        const form = document.querySelector('form#form-login');
+        const loginInput = form.querySelector('input[type="email"]');
+        const passwordInput = form.querySelector('input[type="password"]');
+
+        const divMsgError = document.createElement('div');
+        form.parentNode.insertBefore(divMsgError, form);
+
+        form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+
+            const authData = this.checkLogin(divMsgError, loginInput, passwordInput);
+            if (authData !== undefined) {
+                return;
+            }
+
+            // TODO: вынести часть логики в модель
+            UserModel.makeLogin({login, password})
+                .then(({statusCode, responseObject}) => {
+                    if (statusCode === 200) {
+                        profilePage(this.#parent);
+                    } else {
+                        const {error} = JSON.parse(responseObject);
+                        console.log(error);
+                    }
+                })
+                .catch((err) => {
+                    if (err instanceof Error) {
+                        console.log(err);
+                    }
+                    this.#data.login = true;
+                    this.render();
+                });
+        });
+
+        loginInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkLogin(divMsgError, loginInput, passwordInput);
+        });
+
+        passwordInput.addEventListener('blur', (evt) => {
+            evt.preventDefault();
+
+            while (divMsgError.firstChild) {
+                divMsgError.removeChild(divMsgError.lastChild);
+            }
+            this.checkLogin(divMsgError, loginInput, passwordInput);
+        });
+    }
+
+}
