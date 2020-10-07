3ecc2e6f7b985bd601642cef6f2dc95ac0297906
rufa-a <roofinda@gmail.com>
2020-10-07 14:10:33 +0300

    added model for article

---
+++ static/js/models/article-model.js
@@ -0,0 +1,36 @@
+import Model from "../api/model.js"
+
+/**
+ * TODO:
+ * перенести часть логики обработки запросов
+ * из соответствубщих компонентов (контроллеров)
+ */
+export default class ArticleModel extends Model {
+    constructor() {
+        super();
+    }
+    
+    static getUserArticles(id) {
+        return Fetching.queryGet({
+            url: urls.getUserArticles,
+        });
+    }
+
+    static makeArticle({
+                          title = '',
+                          text = '',
+                      }) {
+        return Fetching.queryPost({
+            url: urls.makeArticle,
+            body: JSON.stringify({
+                'title': title,
+                'text': text,
+            }),
+            headers: {
+                'X-CSRF-TOKEN': Fetching.getCookie('X-CSRF-TOKEN'),
+                'Content-Type': 'application/json',
+            },
+        });
+    }
+    
+}
